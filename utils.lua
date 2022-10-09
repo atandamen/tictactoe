006d954285f8f974ae8fda6515993dab4ce3f101
at@nda <irbisapp54@gmail.com>
2022-10-09 21:53:09 +0300

    * added utility package

--- utils.lua
+++ utils.lua
@@ -1,17 +1,42 @@
-return {
-	inrange = function(tbl, n, m, soft)
-		soft = soft or true
-		for _, v in pairs(tbl) do
-			if (soft) then
-				if not (v >= n and v <= m) then 
-					return false 
-				end
-			else
-				if not (v > n and v < m) then 
-					return false 
-				end
+
+local inrange_ = function(tbl, n, m, soft)
+	soft = soft or true
+	for _, v in pairs(tbl) do
+		if (soft) then
+			if not (v >= n and v <= m) then 
+				return false 
+			end
+		else
+			if not (v > n and v < m) then 
+				return false 
 			end
 		end
-		return true		
 	end
+	return true		
+end 
+
+local getOsName_ = function()
+	--/ hack 
+	local sep = package.config:sub(1, 1)
+	if sep == '/' then return "Unix"
+	elseif sep == '\\' then return "Windows"
+	end
+end
+
+local isUnix_ = function() return getOsName_() == "Unix" end
+
+local isWindows_ = function() return getOsName_() == "Windows" end
+
+local cls_ = function() 
+	local cmd = isUnix_() and "clear" or "cls"
+	os.execute(cmd)
+end
+
+
+return {
+	inrange = inrange_,
+	getOsName = getOsName_,
+	isWindows = isWindows_,
+	isUnix = isUnix_,
+	cls = cls_
 }
